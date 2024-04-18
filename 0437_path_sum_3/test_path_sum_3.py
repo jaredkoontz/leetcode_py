f7a38637cc48be9967c55d048a1ebdba23a9c4ac
jared <jkoontz09@gmail.com>
2024-04-17 18:52:15 -0600

    even more dfs (yet even more)

---
+++ 0437_path_sum_3/test_path_sum_3.py
@@ -0,0 +1,104 @@
+from collections import deque
+
+import pytest
+
+from helpers.bin_tree import create_tree
+from helpers.bin_tree import TreeNode
+
+
+class Solution:
+    def pathSum(self, root: TreeNode | None, targetSum: int) -> int:
+        return self.pathSum_recursive(root, targetSum)
+
+    def pathSum_queue(self, root: TreeNode | None, targetSum: int) -> int:
+        if root is None:
+            return 0
+        queue = [(root, [])]
+        res = 0
+        while queue:
+            node, ans = queue.pop(0)
+            curr_ans = []
+            if node.val == targetSum:
+                res += 1
+            for val in ans:
+                sum_val = val + node.val
+                if sum_val == targetSum:
+                    res += 1
+                curr_ans.append(sum_val)
+            curr_ans.append(node.val)
+            if node.left:
+                queue.append((node.left, curr_ans))
+            if node.right:
+                queue.append((node.right, curr_ans))
+        return res
+
+    # todo fails
+    @staticmethod
+    def pathSum_mine(root: TreeNode | None, targetSum: int) -> int:
+        if root is None:
+            return 0
+
+        queue = deque([(root, root.val, {})])
+        all_sums = 0
+
+        while queue:
+            node, path_sum, local_cache = queue.popleft()
+            node_val = node.val
+            # path_sum += node_val
+            # node itself equals
+            if node_val == targetSum:
+                all_sums += 1
+
+            old_path_sum = path_sum - targetSum
+            # update result and cache
+            all_sums += local_cache.get(old_path_sum, 0)
+            # todo updates cache for all nodes in queue
+            local_cache[path_sum] = local_cache.get(path_sum, 0) + 1
+
+            if node.left:
+                queue.append((node.left, node.left.val, local_cache))
+            if node.right:
+                queue.append((node.right, node.right.val, local_cache))
+            # local_cache[path_sum] -= 1
+
+        return all_sums
+
+    RECURSIVE_RESULT = 0
+
+    def pathSum_recursive(self, root, target):
+        # define global result and path
+        cache = {0: 1}
+
+        def dfs(node, current_target, curr_path_sum, my_cache):
+            # exit condition
+            if node is None:
+                return
+                # calculate currPathSum and required old_path_sum
+            curr_path_sum += node.val
+            old_path_sum = curr_path_sum - current_target
+            # update result and cache
+            self.RECURSIVE_RESULT += my_cache.get(old_path_sum, 0)
+            my_cache[curr_path_sum] = my_cache.get(curr_path_sum, 0) + 1
+
+            # dfs breakdown
+            dfs(node.left, current_target, curr_path_sum, my_cache)
+            dfs(node.right, current_target, curr_path_sum, my_cache)
+            # when move to a different branch, the currPathSum is no longer available, hence remove one.
+            my_cache[curr_path_sum] -= 1
+
+        # recursive to get result
+        dfs(root, target, 0, cache)
+
+        # return result
+        return self.RECURSIVE_RESULT
+
+
+@pytest.mark.parametrize(
+    "l1,target_sum,expected",
+    [
+        ([10, 5, -3, 3, 2, None, 11, 3, -2, None, 1], 8, 3),
+        ([5, 4, 8, 11, None, 13, 4, 7, 2, None, None, 5, 1], 22, 3),
+    ],
+)
+def test_path_sum_3(l1, target_sum, expected):
+    assert Solution().pathSum(create_tree(l1), target_sum) == expected
---
+++ 0988_smallest_string_from_leaf/test_smallest_string_from_leaf.py
@@ -0,0 +1,51 @@
+import string
+from collections import deque
+
+import pytest
+
+from helpers.bin_tree import create_tree
+from helpers.bin_tree import TreeNode
+
+
+LETTER_MAPPING = {x: y for x, y in zip(range(0, 26), string.ascii_lowercase)}
+
+
+class Solution:
+    @staticmethod
+    def smallestFromLeaf(root: TreeNode | None) -> str:
+        def _calculate_lex_path(path: list[int]) -> str:
+            my_str = ""
+            for char in reversed(path):
+                my_str += LETTER_MAPPING[char]
+            return my_str
+
+        min_str = "zzzzzzzzzzzzzzzzzzzzzzzzzz"
+
+        if not root:
+            return ""
+
+        queue = deque()
+        queue.append((root, root.val, [root.val]))
+        while queue:
+            node, val, curr_path = queue.popleft()
+            if not node.left and not node.right:
+                curr_lex_path = _calculate_lex_path(curr_path)
+                if curr_lex_path < min_str:
+                    min_str = curr_lex_path
+            if node.left:
+                queue.append((node.left, node.val, curr_path + [node.left.val]))
+            if node.right:
+                queue.append((node.right, node.val, curr_path + [node.right.val]))
+        return min_str
+
+
+@pytest.mark.parametrize(
+    "l1,expected",
+    [
+        ([0, 1, 2, 3, 4, 3, 4], "dba"),
+        ([25, 1, 3, 1, 3, 0, 2], "adz"),
+        ([2, 2, 1, None, 1, 0, None, 0], "abc"),
+    ],
+)
+def test_smallest_from_leaf(l1, expected):
+    assert Solution().smallestFromLeaf(create_tree(l1)) == expected

b8c01e31aa85df2051442a4e1405aa156992d0c6
jared <jkoontz09@gmail.com>
2024-04-23 16:41:56 -0600

    can jump

---
+++ 0055_can_jump/test_can_jump.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+class Solution:
+    def canJump(self, nums: list[int]) -> bool:
+        n = len(nums)
+        goal = n - 1
+
+        for i in range(n - 2, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+
+        return goal == 0
+
+
+@pytest.mark.parametrize(
+    "nums, expected", [([2, 3, 1, 1, 4], True), ([3, 2, 1, 0, 4], False)]
+)
+def test_canJump(nums, expected):
+    assert Solution().canJump(nums) == expected

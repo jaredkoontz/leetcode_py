2d4398d2eaab676e7a6c626948b4b2efbffe5445
jared <jkoontz09@gmail.com>
2024-04-23 16:17:38 -0600

    more from saved later

---
+++ 0079_word_search/test_word_search.py
@@ -0,0 +1,62 @@
+import pytest
+
+# more at https://leetcode.com/problems/word-search/solutions/27660/python-dfs-solution-with-comments/
+
+
+class Solution:
+    def exist(self, board: list[list[str]], word: str) -> bool:
+        self.ROWS = len(board)
+        self.COLS = len(board[0])
+        self.board = board
+
+        for row in range(self.ROWS):
+            for col in range(self.COLS):
+                if self.backtrack(row, col, word):
+                    return True
+
+        return False
+
+    def backtrack(self, row, col, suffix):
+        if len(suffix) == 0:
+            return True
+
+        if (
+            row < 0
+            or row == self.ROWS
+            or col < 0
+            or col == self.COLS
+            or self.board[row][col] != suffix[0]
+        ):
+            return False
+        ret = False
+        self.board[row][col] = "#"
+        for row_offset, col_offset in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            ret = self.backtrack(row + row_offset, col + col_offset, suffix[1:])
+            if ret:
+                break
+        self.board[row][col] = suffix[0]
+        return ret
+
+
+@pytest.mark.parametrize(
+    "board, word, expected",
+    [
+        (
+            [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]],
+            "ABCCED",
+            True,
+        ),
+        (
+            [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]],
+            "SEE",
+            True,
+        ),
+        (
+            [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]],
+            "ABCB",
+            False,
+        ),
+    ],
+)
+def test_exist(board, word, expected):
+    assert Solution().exist(board, word) == expected
---
+++ 0733_flood_fill/test_flood_fill.py
@@ -0,0 +1,38 @@
+import pytest
+
+# todo more at https://leetcode.com/problems/flood-fill/solutions/109604/easy-python-dfs-no-need-for-visited/
+
+
+class Solution:
+    def floodFill(
+        self, image: list[list[int]], sr: int, sc: int, color: int
+    ) -> list[list[int]]:
+        m, n = len(image), len(image[0])
+        start_color = image[sr][sc]
+
+        if color == start_color:
+            return image
+
+        def dfs(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n or image[i][j] != start_color:
+                return
+
+            image[i][j] = color
+            dfs(i - 1, j)
+            dfs(i + 1, j)
+            dfs(i, j - 1)
+            dfs(i, j + 1)
+
+        dfs(sr, sc)
+        return image
+
+
+@pytest.mark.parametrize(
+    "image,sr,sc,color,expected",
+    [
+        ([[1, 1, 1], [1, 1, 0], [1, 0, 1]], 1, 1, 2, [[2, 2, 2], [2, 2, 0], [2, 0, 1]]),
+        ([[0, 0, 0], [0, 0, 0]], 0, 0, 0, [[0, 0, 0], [0, 0, 0]]),
+    ],
+)
+def test_floodFill(image, sr, sc, color, expected):
+    assert Solution().floodFill(image, sr, sc, color) == expected

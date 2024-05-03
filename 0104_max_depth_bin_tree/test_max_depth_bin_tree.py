a143744327f969bf6a37578836a32184c7ae9313
jared <jkoontz09@gmail.com>
2024-05-03 16:02:28 -0600

    max_depth_bin_tree, product_of_array

---
+++ 0104_max_depth_bin_tree/test_max_depth_bin_tree.py
@@ -0,0 +1,77 @@
+import collections
+
+import pytest
+
+from helpers.bin_tree import make_tree
+from helpers.bin_tree import TreeNode
+
+
+class Solution:
+    def maxDepth(self, root: TreeNode | None) -> int:
+        assert (
+            self.maxDepth_bfs(root)
+            == self.maxDepth_dfs(root)
+            == self.maxDepth_rec(root)
+        )
+        return self.maxDepth_bfs(root)
+
+    @staticmethod
+    def maxDepth_bfs(root: TreeNode | None) -> int:
+        if not root:
+            return 0
+
+        queue = collections.deque()
+        curr_max = -1
+        if root:
+            queue.append((root, 1))
+
+        while queue:
+            node, depth = queue.popleft()
+            curr_max = max(curr_max, depth)
+
+            if node.left:
+                queue.append((node.left, depth + 1))
+            if node.right:
+                queue.appendleft((node.right, depth + 1))
+
+        return curr_max
+
+    @staticmethod
+    def maxDepth_dfs(root: TreeNode | None) -> int:
+        if not root:
+            return 0
+
+        stack = [(root, 1)]
+        max_depth = 0
+
+        while stack:
+            node, depth = stack.pop()
+            max_depth = max(max_depth, depth)
+
+            if node.left:
+                stack.append((node.left, depth + 1))
+            if node.right:
+                stack.append((node.right, depth + 1))
+
+        return max_depth
+
+    @staticmethod
+    def maxDepth_rec(root: TreeNode | None) -> int:
+        def helper(node):
+            if not node:
+                return 0
+            return 1 + max(helper(node.left), helper(node.right))
+
+        return helper(root)
+
+
+@pytest.mark.parametrize(
+    "input_list,expected",
+    [
+        ([3, 9, 20, None, None, 15, 7], 3),
+        ([1, None, 2], 2),
+        ([1, 2, 3, 4, None, None, 5], 3),
+    ],
+)
+def test_maxDepth(input_list, expected):
+    assert Solution().maxDepth(make_tree(input_list)) == expected
---
+++ 0238_product_of_array/test_product_of_array.py
@@ -0,0 +1,124 @@
+import pytest
+
+
+class Solution:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        assert (
+            self.productExceptSelf_pre_suf_mine(nums)
+            == self.productExceptSelf_pre_suf_optimal(nums)
+            == self.productExceptSelf_pre_suf(nums)
+            == self.productExceptSelf_brute_force(nums)
+            == self.productExceptSelf_neet(nums)
+            == self.productExceptSelf_final(nums)
+        )
+        return self.productExceptSelf_final(nums)
+
+    @staticmethod
+    def productExceptSelf_final(nums: list[int]) -> list[int]:
+        length = len(nums)
+        products = [1] * length
+        for i in range(1, length):
+            products[i] = nums[i - 1] * products[i - 1]
+
+        right = nums[-1]
+        for i in range(length - 2, -1, -1):
+            products[i] *= right
+            right *= nums[i]
+        return products
+
+    @staticmethod
+    def productExceptSelf_neet(nums: list[int]) -> list[int]:
+        res = [1] * len(nums)
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+
+    @staticmethod
+    def productExceptSelf_pre_suf_mine(nums: list[int]) -> list[int]:
+        n = len(nums)
+        left = [0] * n
+        right = [0] * n
+        l_pointer = 1
+        r_pointer = 1
+
+        for i, num in enumerate(nums):
+            left[i] = l_pointer
+            j = -i - 1
+            right[j] = r_pointer
+            l_pointer *= nums[i]
+            r_pointer *= nums[j]
+
+        return [l_pointer * r_pointer for l_pointer, r_pointer in zip(left, right)]
+
+    @staticmethod
+    def productExceptSelf_pre_suf_optimal(nums: list[int]) -> list[int]:
+        n = len(nums)
+        ans = [1] * n  # Initialize the result array with 1
+
+        curr = 1
+        # Calculate prefix product and store directly in ans
+        for i in range(n):
+            ans[i] = curr
+            curr *= nums[i]
+
+        curr = 1
+        # Calculate suffix product and multiply with prefix to get the final answer
+        for i in range(n - 1, -1, -1):
+            ans[i] *= curr
+            curr *= nums[i]
+
+        return ans
+
+    @staticmethod
+    def productExceptSelf_pre_suf(nums: list[int]) -> list[int]:
+        n = len(nums)
+
+        # Initialize prefix and suffix arrays
+        pre = [1] * n
+        suff = [1] * n
+
+        # Calculate prefix product
+        for i in range(1, n):
+            pre[i] = pre[i - 1] * nums[i - 1]
+
+        # Calculate suffix product
+        for i in range(n - 2, -1, -1):
+            suff[i] = suff[i + 1] * nums[i + 1]
+
+        # Combine prefix and suffix products to get the result
+        ans = [pre[i] * suff[i] for i in range(n)]
+
+        return ans
+
+    @staticmethod
+    def productExceptSelf_brute_force(nums: list[int]) -> list[int]:
+        n = len(nums)
+        ans = [0] * n
+
+        for i in range(n):
+            product = 1
+            for j in range(n):
+                if i == j:
+                    continue
+                product *= nums[j]
+            ans[i] = product
+
+        return ans
+
+
+@pytest.mark.parametrize(
+    "nums,expected",
+    [
+        ([1, 2, 3, 4], [24, 12, 8, 6]),
+        ([-1, 1, 0, -3, 3], [0, 0, 9, 0, 0]),
+        ([1, 2], [2, 1]),
+    ],
+)
+def test_product_of_array(nums, expected):
+    assert Solution().productExceptSelf(nums) == expected

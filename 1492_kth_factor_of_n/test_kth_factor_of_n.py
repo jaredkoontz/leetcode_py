138b97b497642e0e4a7f1a3d90dc9c43c915e25c
jared <jkoontz09@gmail.com>
2024-05-21 21:56:50 -0600

    kth_factor_of_n, optimal_partition_str

---
+++ 1492_kth_factor_of_n/test_kth_factor_of_n.py
@@ -0,0 +1,29 @@
+import pytest
+
+
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        counter = 1
+        for i in range(1, (n // 2) + 1):
+            if n % i == 0:
+                counter += 1
+                if counter == k + 1:
+                    return i
+        if counter == k:
+            return n
+        return -1
+
+
+@pytest.mark.parametrize(
+    "n,k,expected",
+    [
+        (12, 3, 3),
+        (7, 2, 7),
+        (4, 4, -1),
+        (5, 4, -1),
+        (7, 4, -1),
+        (4, 2, 2),
+    ],
+)
+def test_kth_factor(n, k, expected):
+    assert Solution().kthFactor(n, k) == expected
---
+++ 2405_optimal_partition_str/test_optimal_partition.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+class Solution:
+    def partitionString(self, s: str) -> int:
+        if not s:
+            return 0
+        num_ways = 0
+        curr_set = set()
+        for ch in s:
+            if ch in curr_set:
+                num_ways += 1
+                curr_set.clear()
+            curr_set.add(ch)
+        return num_ways + 1 if curr_set else num_ways
+
+
+@pytest.mark.parametrize(
+    "s,expected",
+    [
+        ("abacaba", 4),
+        ("ssssss", 6),
+    ],
+)
+def test_optimal_partition(s, expected):
+    assert Solution().partitionString(s) == expected

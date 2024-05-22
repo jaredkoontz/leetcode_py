ee5e255d65f94680048773f13553d6e3135fe82d
jared <jkoontz09@gmail.com>
2024-05-21 22:15:24 -0600

    convert_sorted_array_bst

---
+++ 0108_convert_sorted_array_bst/test_convert_sorted_array_bst.py
@@ -0,0 +1,29 @@
+import pytest
+
+from helpers.bin_tree import create_array_from_tree
+from helpers.bin_tree import TreeNode
+
+
+class Solution:
+    def sortedArrayToBST(self, nums: list[int]) -> TreeNode | None:
+        def bst_helper(left, right):
+            if left > right:
+                return None
+            middle = (left + right) // 2
+            root = TreeNode(nums[middle])
+            root.left = bst_helper(left, middle - 1)
+            root.right = bst_helper(middle + 1, right)
+            return root
+
+        return bst_helper(0, len(nums) - 1)
+
+
+@pytest.mark.parametrize(
+    "l1,expected",
+    [
+        ([-10, -3, 0, 5, 9], [0, -10, 5, -3, 9]),
+        ([1, 3], [1, 3]),
+    ],
+)
+def test_sortedArrayToBST(l1, expected):
+    assert create_array_from_tree(Solution().sortedArrayToBST(l1)) == expected

b9d33ad1ef09d8bdac37a954c9e4be1af0fec347
jared <jkoontz09@gmail.com>
2024-06-04 17:37:38 -0600

    0409_longest_palindrome

---
+++ 0409_longest_palindrome/test_longest_palindrome.py
@@ -0,0 +1,40 @@
+from collections import Counter
+
+import pytest
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        counter = Counter(s)
+        longest_palindrome = 0
+        for count in counter.values():
+            if count & 1 == 0:
+                longest_palindrome += count
+            else:
+                longest_palindrome += count-1 if longest_palindrome & 1 else count
+        return longest_palindrome
+
+
+@pytest.mark.parametrize(
+    "s,expected",
+    [
+        ("abccccdd", 7),
+        ("bb", 2),
+        ("a", 1),
+        (
+            "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattle"
+            "fiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivest"
+            "hatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecann"
+            "otconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourp"
+            "oorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhatthey"
+            "didhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonob"
+            "lyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetake"
+            "increaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthese"
+            "deadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeopleb"
+            "ythepeopleforthepeopleshallnotperishfromtheearth",
+            983,
+        ),
+    ],
+)
+def test_longest_palindrome(s, expected):
+    assert Solution().longestPalindrome(s) == expected

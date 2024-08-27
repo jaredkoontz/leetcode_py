ac2b5555fbee1843d80b78e3f136495f4784c03e
jared <jkoontz09@gmail.com>
2024-08-26 20:22:52 -0600

    1514_path_max_prob and iter solutions for bin tree postorder

--- 0145_bin_tree_postorder_traversal/test_postorder_traversal.py
+++ 0145_bin_tree_postorder_traversal/test_postorder_traversal.py
@@ -7,15 +7,118 @@ from helpers.bin_tree import TreeNode
 
 class Solution:
     def postorderTraversal(self, root: TreeNode | None) -> list[int]:
-        my_list = []
-        self.postorder(root, my_list)
-        return my_list
+        return self.postorderTraversal_recursive(root)
 
-    def postorder(self, root, my_list):
-        if root:
-            self.postorder(root.left, my_list)
-            self.postorder(root.right, my_list)
-            my_list.append(root.val)
+    @staticmethod
+    def postorderTraversal_stack(root: TreeNode | None) -> list[int]:
+        result = []
+
+        # If the root is null, return an empty list
+        if root is None:
+            return result
+
+        # To keep track of the previously processed node
+        previous_node = None
+        # Stack to manage the traversal
+        traversal_stack = []
+
+        # Process nodes until both the root is null and the stack is empty
+        while root is not None or len(traversal_stack) > 0:
+            # Traverse to the leftmost node
+            if root is not None:
+                traversal_stack.append(root)
+                root = root.left
+            else:
+                # Peek at the top node of the stack
+                root = traversal_stack[-1]
+
+                # If there is no right child or the right child was already processed
+                if root.right is None or root.right == previous_node:
+                    result.append(root.val)
+                    traversal_stack.pop()
+                    previous_node = root
+                    root = None  # Ensure we don’t traverse again from this node
+                else:
+                    # Move to the right child
+                    root = root.right
+
+        return result
+
+    @staticmethod
+    def postorderTraversal_two_stack(root: TreeNode | None) -> list[int]:
+        result = []
+
+        # If the root is null, return an empty list
+        if root is None:
+            return result
+
+        # Stack to manage the traversal
+        main_stack = []
+        # Stack to manage the path
+        path_stack = []
+
+        # Start with the root node
+        main_stack.append(root)
+
+        # Process nodes until the main stack is empty
+        while main_stack:
+            root = main_stack[-1]
+
+            # If the node is in the path stack and it's the top, add its value
+            if path_stack and path_stack[-1] == root:
+                result.append(root.val)
+                main_stack.pop()
+                path_stack.pop()
+            else:
+                # Push the current node to the path stack
+                path_stack.append(root)
+                # Push right child if it exists
+                if root.right is not None:
+                    main_stack.append(root.right)
+                # Push left child if it exists
+                if root.left is not None:
+                    main_stack.append(root.left)
+
+        return result
+
+    @staticmethod
+    def postorderTraversal_stack_trick(root: TreeNode | None) -> list[int]:
+        # List to store the result of postorder traversal
+        result = []
+        # Stack to facilitate the traversal of nodes
+        traversal_stack = []
+        current_node = root
+
+        # Traverse the tree while there are nodes to process
+        while current_node or traversal_stack:
+            if current_node:
+                # Add current node's value to result list before going to its children
+                result.append(current_node.val)
+                # Push current node onto the stack
+                traversal_stack.append(current_node)
+                # Move to the right child
+                current_node = current_node.right
+            else:
+                # Pop the node from the stack and move to its left child
+                current_node = traversal_stack.pop()
+                current_node = current_node.left
+        # Reverse the result list to get the correct postorder sequence
+        result.reverse()
+        return result
+
+    @staticmethod
+    def postorderTraversal_recursive(root: TreeNode | None) -> list[int]:
+        nodes = []
+
+        def post_order(node):
+            if node:
+                post_order(node.left)
+                post_order(node.right)
+                nodes.append(node.val)
+            return
+
+        post_order(root)
+        return nodes
 
 
 @pytest.mark.parametrize(
---
+++ 1514_path_max_prob/test_path_max_prob.py
@@ -0,0 +1,106 @@
+# https://leetcode.com/problems/path-with-maximum-probability
+import heapq
+from collections import defaultdict
+from collections import deque
+
+import pytest
+
+
+class State:
+    def __init__(self, node: int, prob: float):
+        self.node = node
+        self.prob = prob
+
+    def __str__(self):
+        return f"{self.node=} {self.prob=}"
+
+    def __lt__(self, other):
+        # Reverse order to create a max-heap based on probability
+        return self.prob > other.prob
+
+
+def _create_graph(edges: list[list[int]], succProb: list[float]):
+    graph = defaultdict(list)
+    for i, (u, v) in enumerate(edges):
+        graph[u].append((v, succProb[i]))
+        graph[v].append((u, succProb[i]))
+    return graph
+
+
+class Solution:
+    def maxProbability(
+        self,
+        n: int,
+        edges: list[list[int]],
+        succProb: list[float],
+        start_node: int,
+        end_node: int,
+    ) -> float:
+        return self.maxProbability_bfs(n, edges, succProb, start_node, end_node)
+
+    @staticmethod
+    def maxProbability_dijkstra(
+        n: int,
+        edges: list[list[int]],
+        succProb: list[float],
+        start_node: int,
+        end_node: int,
+    ) -> float:
+        graph = _create_graph(edges, succProb)
+
+        probs = [0.0] * n
+        probs[start_node] = 1.0
+        pq = []
+        heapq.heappush(pq, State(start_node, 1.0))
+
+        while pq:
+            state = heapq.heappop(pq)
+            parent = state.node
+            prob = state.prob
+
+            if parent == end_node:
+                return prob
+
+            for child, edge_prob in graph[parent]:
+                new_prob = prob * edge_prob
+                if new_prob > probs[child]:
+                    probs[child] = new_prob
+                    heapq.heappush(pq, State(child, new_prob))
+
+        return 0.0
+
+    @staticmethod
+    def maxProbability_bfs(
+        n: int,
+        edges: list[list[int]],
+        succProb: list[float],
+        start_node: int,
+        end_node: int,
+    ) -> float:
+        graph = _create_graph(edges, succProb)
+        probs = [0.0] * n
+        queue = deque([State(start_node, 1.0)])
+
+        while queue:
+            state = queue.popleft()
+            parent, prob = state.node, state.prob
+
+            for neighbor, edge_prob in graph[parent]:
+                new_prob = prob * edge_prob
+                if new_prob > probs[neighbor]:
+                    probs[neighbor] = new_prob
+                    queue.append(State(neighbor, new_prob))
+
+        return probs[end_node]
+
+
+@pytest.mark.parametrize(
+    "n,edges,succProb,start,end,expected",
+    [
+        (3, [[0, 1], [1, 2], [0, 2]], [0.5, 0.5, 0.2], 0, 2, 0.25000),
+        (3, [[0, 1], [1, 2], [0, 2]], [0.5, 0.5, 0.3], 0, 2, 0.30000),
+        (3, [[0, 1]], [0.5], 0, 2, 0.00000),
+    ],
+)
+def test_maxProbability(n, edges, succProb, start, end, expected):
+    assert Solution().maxProbability(n, edges, succProb, start, end) == expected

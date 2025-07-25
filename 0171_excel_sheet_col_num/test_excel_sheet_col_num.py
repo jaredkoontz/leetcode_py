ba352ea017ca256982cff9edc883110156b3355d
Jared Koontz <jkoontz09@gmail.com>
2025-07-25 13:57:36 -0600

    0171_excel_sheet_col_num, 0461_hamming_distance

---
+++ 0171_excel_sheet_col_num/test_excel_sheet_col_num.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        return self.titleToNumber_mine(columnTitle)
+    @staticmethod
+    def titleToNumber_mine(columnTitle: str) -> int:
+        result = 0
+        for i in range(len(columnTitle)):
+            c = columnTitle[i]
+            result = result * 26 + (ord(c) - ord('A') + 1)
+        return result
+
+
+@pytest.mark.parametrize("columnTitle,expected", [
+    ("A", 1),
+    ("B", 2),
+    ("AA", 27),
+    ("AB", 28),
+    ('ZY',701)
+])
+def test_titleToNumber(columnTitle, expected) -> None:
+    assert Solution().titleToNumber(columnTitle) == expected
---
+++ 0461_hamming_distance/test_hamming_distance.py
@@ -0,0 +1,36 @@
+import pytest
+
+
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        return self.hammingDistance_mine(x, y)
+
+    @staticmethod
+    def hammingDistance_smarter(x: int, y: int) -> int:
+        xor = x ^ y
+        distance = 0
+        while xor:
+            distance += 1
+            xor = xor & (xor-1)
+        return distance
+
+    @staticmethod
+    def hammingDistance_mine(x: int, y: int) -> int:
+        hamming_distance = 0
+        while x > 0 or y > 0:
+            last_x = x & 1
+            last_y = y & 1
+            if last_x ^ last_y:
+                hamming_distance += 1
+            x >>= 1
+            y >>= 1
+        return hamming_distance
+
+@pytest.mark.parametrize("x,y,expected", [
+    (1, 1, 0),
+    (1, 4, 2),
+    (3, 1, 1),
+    (3,0,2),
+])
+def test_hamming_distance(x, y, expected):
+    assert Solution().hammingDistance(x, y) == expected

fc72f3a7483302394ee749fd471bb92ec577d9e5
Jared Koontz <jkoontz09@gmail.com>
2025-07-25 16:39:31 -0600

    0191_num_1_bits, 0258_add_digits

---
+++ 0258_add_digits/test_add_digits.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/add-digits
+import pytest
+
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        return self.addDigits_rec(num)
+
+    @staticmethod
+    def addDigits_rec(num: int) -> int:
+        def helper(my_num):
+            if my_num < 10:
+                return my_num
+
+            digits = []
+            while my_num > 10:
+                digits.append(my_num % 10)
+                my_num //= 10
+            digits.append(my_num)
+            return helper(sum(digits))
+
+        return helper(num)
+
+
+@pytest.mark.parametrize("num,expected", [(38, 2), (0, 0), (1, 1), (11, 2), (9999, 9)])
+def test_addDigits(num: int, expected: int):
+    assert Solution().addDigits(num) == expected
